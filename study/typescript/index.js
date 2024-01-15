1e77a553cb39e4ab1e24620950cd189cc6466b28
방미나 <>
2024-01-15 22:49:03 +0900

    240115 타입스크립트 강의

--- study/typescript/index.js
+++ study/typescript/index.js
@@ -1,25 +1,15 @@
-// function 내함수(x: number | string) {
-//   // Type이 아직 하나로 확정되지 않았을 경우 Type Narrowing이라는 스킬을 써야함
-//   // 타입스크립트에서 지켜야할 코딩방식
-//   // union type등 때문에 어떤 변수가 타입이 아직 불확실하면
-//   // if문 등으로 narrowing 해줘야 조작 가능
-//   // if문 썼으면 끝까지 써야 안전 else, else if 안쓰면 에러로 잡을 수 있음
-//   if (typeof x === 'string') {
-//     return x + 1
-//   } else {
-//     return x + 1
-//   }
-// }
-// 내함수(123)
-function 내함수(x) {
-    var array = [];
-    // assertion 문법 : 타입을 잠깐 덮어 씌우는 것, assertion 문법은 if문 필요없음
-    array[0] = x; // 왼쪽에 있는 변수로 number로 덮어 씌워라
-    // as 문법의 용도
-    // 1. narrowing 할 때. 유니온타입을 하나의 타입으로 확장하고 싶을 때
-    // 2. 무슨 타입이 들어올지 100% 확실할 때 쓰기
-}
-function 내함수1(x) {
-    return x + 1;
-}
-console.log(내함수1('123'));
+// 함수표현식에만 type alias 사용가능
+var 함수 = function (a) {
+    return 10;
+};
+var 회원정보 = {
+    name: 'kim',
+    plusOne: function (a) {
+        return a + 1;
+        // 파라미터 number, return number 타입지정 해주기
+    },
+};
+회원정보.plusOne(1);
+// 콜백함수 개념
+function 함수1() { }
+function 함수2() { }
