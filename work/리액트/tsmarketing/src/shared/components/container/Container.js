d8fd49b12e6f29229d3b4094d62bff87bf7ce178
bangmina <suu5868@gmail.com>
2024-07-05 15:49:12 +0900

    240705 commit

--- work/리액트/tsmarketing/src/shared/components/container/Container.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./container.scss";
-
-const Container = ({ children, sectionName }) => {
-	return <section className={`container ${sectionName}`}>{children}</section>;
-};
-
-export default Container;
--- work/리액트/tsmarketing/src/shared/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Experience } from "./experience/Experience";
-export { default as MarketingInquire } from "./marketingInquire/MarketingInquire";
-export { default as Introduce } from "./introduce/Introduce";
-export { default as Search } from "./search/Search";
-export { default as SmartStore } from "./smartStore/SmartStore";
